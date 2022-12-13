73cdaba4363563fc502f599d97c7bdf3f5902aa5
karlinexcode <etiniarchibong02@gmail.com>
2022-12-13 04:19:23 -0800

    ok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * Main - prints _putchar with a new line
+ * main - prints _putchar with a new line
  *
  * Return: always 0 (success)
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	printf("_putchar\n");
 	return (0);
 }
